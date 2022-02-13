1586b0917fbacaafc81bdcff658392ab82743f01
arica-c <arica.chakraborty@khoros.com>
2022-02-13 13:17:50 +0530

    C104: Implemented the Rest Endpoints

--- src/main/java/com/twitter/starter/AddingTweets.java
+++ src/main/java/com/twitter/starter/AddingTweets.java
@@ -1,27 +1,31 @@
+package com.twitter.starter;
 
+import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
-public class ProgramTwo {
-    public static void main(String[] args){
-        try{
+public class AddingTweets {
+    public String addingTweet(String args) {
+        try {
             Properties prop = new Properties();
             String propFileName = "application.properties";
 
-            InputStream inputStream = ProgramOne.class.getClassLoader().getResourceAsStream(propFileName);
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
 
             if (inputStream != null) {
                 prop.load(inputStream);
             } else {
                 throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
             }
+
+            //Getting the properties from file
             String consmerKey = prop.getProperty("consumerKey");
             String consumerSecret = prop.getProperty("consumerSecret");
             String accessToken = prop.getProperty("accessToken");
@@ -35,17 +39,16 @@ public class ProgramTwo {
             TwitterFactory tf = new TwitterFactory(cb.build());
             Twitter twitter = tf.getInstance();
 
-                List<String> list = twitter.getHomeTimeline().stream()
-                        .map(item -> item.getText())
-                        .collect(Collectors.toList());
 
+            Status status = twitter.updateStatus(args);
+            String str = status.getText();
+            return str;
+        } catch (TwitterException e) {
 
-                for (String s : list) {
-                    System.out.println(s);
-                }
+            return "Check your access tokens";
         }
-        catch(Exception e){
-            System.out.println(e.toString());
+        catch(IOException e){
+            return "Exception raised while reading the application file";
         }
     }
 }
--- src/main/java/com/twitter/starter/GettingTimeline.java
+++ src/main/java/com/twitter/starter/GettingTimeline.java
@@ -1,31 +1,31 @@
+package com.twitter.starter;
 
-import twitter4j.conf.ConfigurationBuilder;
-import twitter4j.TwitterFactory;
 import twitter4j.Twitter;
-import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
-public class ProgramOne {
-    //program to post a tweet
-
-
-    public static void main(String[] args) {
-        try {
+public class GettingTimeline {
+    public List<String> getTimeline(){
+        try{
             Properties prop = new Properties();
             String propFileName = "application.properties";
 
-            InputStream inputStream = ProgramOne.class.getClassLoader().getResourceAsStream(propFileName);
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
 
             if (inputStream != null) {
                 prop.load(inputStream);
             } else {
                 throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
             }
-
-            //Getting the properties from file
             String consmerKey = prop.getProperty("consumerKey");
             String consumerSecret = prop.getProperty("consumerSecret");
             String accessToken = prop.getProperty("accessToken");
@@ -39,19 +39,19 @@ public class ProgramOne {
             TwitterFactory tf = new TwitterFactory(cb.build());
             Twitter twitter = tf.getInstance();
 
-            StringBuffer s = new StringBuffer();
-            for (int i = 0; i < args.length; i++) {
-                s.append(args[i] + " ");
-            }
-            String string = s.toString();
+            List<String> list = twitter.getHomeTimeline().stream()
+                    .map(item -> item.getText())
+                    .collect(Collectors.toList());
+            return list;
 
-            Status status = twitter.updateStatus(string);
-            String str = status.getText();
-            System.out.println(str);
+        }
+        catch(TwitterException e){
+            return Collections.singletonList("Check your access tokens");
+        }
+        catch(IOException e){
+
+            return Collections.singletonList("Exception raised while reading the application file");
         }
 
-        catch(Exception e){
-            System.out.println(e.toString());
-        }
     }
 }
