dff1038cfa1dae90c70d6054e313f027d3619564
arica-c <arica.chakraborty@khoros.com>
2022-02-14 16:58:40 +0530

    C111: Used dependency injection framework Spring

--- src/main/java/com/twitter/Resources/FilterTimeline.java
+++ src/main/java/com/twitter/Resources/FilterTimeline.java
@@ -3,13 +3,8 @@ package com.twitter.Resources;
 import com.twitter.Services.TwitterConnecter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
-
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
